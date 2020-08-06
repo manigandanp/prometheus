c8fe4ef9f3c5dbf5799a0f38195e8849f87b692e
Manigandan P <manigandan.p@avalara.com>
2020-08-06 22:24:38 +0530

    add new metrics to server

--- python-app/server.py
+++ python-app/server.py
@@ -1,25 +1,50 @@
 import http.server
 import socketserver
 import random
+import time
 
 from prometheus_client import start_http_server
 from prometheus_client import Counter
+from prometheus_client import Gauge
+from prometheus_client import Summary
+from prometheus_client import Histogram
 
-REQUESTS = Counter("python_app_hello_world_requests_total", "Hello World requested.")
-EXCEPTIONS = Counter('python_app_hello_world_exceptions_total', 'Exceptions serving Hello World.')
+REQUESTS = Counter("python_app_hello_world_requests_total",
+                   "Hello World requested.")
+EXCEPTIONS = Counter('python_app_hello_world_exceptions_total',
+                     'Exceptions serving Hello World.')
+SALES = Counter('python_app_hello_world_sales_total', "Euros made serving..")
+
+INPROGRESS = Gauge("python_app_hello_world_inprogress",
+                   "Number of hello world in progress")
+LAST = Gauge("python_app_hello_world_time_seconds",
+             "The last time a hello world was served")
+
+LATENCY = Summary('python_app_hello_world_latency_seconds',
+                  'Time taken for a hello world')
+
+LATENCY_HISTO = Histogram("python_app_hello_world_latency_seconds_hosto",
+                          "Time requests for latency")
 
 PORT = 8001
 
+
 class MyHandler(http.server.BaseHTTPRequestHandler):
-  def do_GET(self):
-    REQUESTS.inc()
-    print("[python_app] Getting request..")
-    # with EXCEPTIONS.count_exceptions():
-    #     if random.random() < 0.2:
-    #         raise Exception
-    self.send_response(200)
-    self.end_headers()
-    self.wfile.write(b"Hello Mani")
+    @EXCEPTIONS.count_exceptions()
+    @INPROGRESS.track_inprogress()
+    @LATENCY.time()
+    @LATENCY_HISTO.time()
+    def do_GET(self):
+        REQUESTS.inc()
+        euros = random.random()
+        time.sleep(int(random.random() * 5))
+        SALES.inc(euros)
+        print("[python_app] Getting request..")
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write("Hello World for {} euros".format(euros).encode())
+        LAST.set_to_current_time()
+
 
 Handler = http.server.SimpleHTTPRequestHandler
 
@@ -27,3 +52,7 @@ with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
     print("serving at port", PORT)
     start_http_server(8000)
     httpd.serve_forever()
+
+
+# Shell command to make requsets for 100 times with delay
+# for i in {1..100}; do echo $i; curl http://192.168.29.228:8001/ ; sleep `expr  $i  % 11`s;  done
